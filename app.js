5d99ecb5dfc2912eb168d5ff62efcd3ab72f6acb
Siddharthposhala <siddharthposhala020@gmail.com>
2023-01-04 11:07:28 +0530

    app.js update

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 /* eslint-disable no-unused-vars */
-const { response } = require("express");
 const express = require("express");
 const app = express();
+
+const csrf = require("tiny-csrf");
+const cookieParser = require("cookie-parser");
+const {
+  AdminAdministrator,
+  Create_election,
+  Create_question,
+  Create_options,
+  Create_voterId,
+} = require("./models");
 
 app.get("/", (request, response) => {
   response.send("hello world");
